aaee2708fe9f58c60a5272bf3158a8a900d9afea
equintee <employes23@gmail.com>
2025-12-20 20:38:53 +0300

    Added on click events to grid

--- src/components/VideoBox.jsx
+++ src/components/VideoBox.jsx
@@ -1,5 +1,6 @@
 import {
   Chip,
+  ClickAwayListener,
   createTheme,
   Grid,
   Stack,
@@ -7,7 +8,11 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import { green } from "@mui/material/colors";
+import { useState } from "react";
+
 function VideoBox({ video }) {
+  const [data, setData] = useState(video);
+  const [update, setUpdate] = useState(false);
   if (!video) {
     return;
   }
@@ -53,91 +58,116 @@ function VideoBox({ video }) {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          position: "relative",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          alignItems: "flex-start",
-          backgroundColor: "rgba(99, 99, 99, 0.14)",
-          border: "3px solid rgba(175, 175, 175, 0.12)",
-          width: "50vh",
-          rowGap: "1vh",
-        }}
-      >
+      <ClickAwayListener onClickAway={() => setUpdate(false)}>
         <Grid
+          container
+          onClick={() => setUpdate(true)}
           sx={{
+            display: "flex",
             position: "relative",
-            width: "100%",
-            display: "inline-block",
-            textAlign: "left",
-            fontSize: "2.5vh",
-            flex: "1 1 auto",
-            paddingLeft: "1%",
-            wordWrap: "break-word",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            alignItems: "flex-start",
+            backgroundColor: update
+              ? "rgba(99, 99, 99, 0.31)"
+              : "rgba(99, 99, 99, 0.14)",
+            border: update
+              ? "3px solid rgba(175, 175, 175, 0.31)"
+              : "3px solid rgba(175, 175, 175, 0.14)",
+            width: "50vh",
+            rowGap: "1vh",
           }}
         >
-          <TextField
-            fullWidth
-            spellCheck={false}
-            multiline
-            value={video.description}
-          ></TextField>
-        </Grid>
-        {video.tags ? (
           <Grid
             sx={{
               position: "relative",
               width: "100%",
-              display: "flex",
-              paddingLeft: "1%",
-              alignItems: "flex-start",
-              flexDirection: "row",
+              display: "inline-block",
+              textAlign: "left",
+              fontSize: "2.5vh",
+              flex: "1 1 auto",
+              wordWrap: "break-word",
             }}
           >
-            {" "}
-            <Stack direction={"row"} sx={{ flexWrap: "wrap" }}>
-              {video.tags.map((tag) => (
-                <Chip label={tag} color="primary" variant="outlined"></Chip>
-              ))}
-            </Stack>
+            <TextField
+              fullWidth
+              spellCheck={false}
+              multiline
+              value={data.description}
+              onChange={(text) => {
+                setData({
+                  ...data,
+                  description: text.target.value,
+                });
+              }}
+            ></TextField>
           </Grid>
-        ) : (
-          <></>
-        )}
-        <Grid
-          sx={{
-            position: "relative",
-            paddingLeft: "1%",
-          }}
-        >
-          🎵 {video.song.name}
-        </Grid>
-        <Grid
-          sx={{
-            position: "relative",
-            width: "100%",
-            height: "80%",
-            display: "flex",
-            justifyContent: "center",
-            flex: "1 1 auto",
-          }}
-        >
-          <video
-            muted
-            controls
-            style={{
-              maxWidth: "100%",
-              width: "auto",
-              height: "auto",
-              objectFit: "contain",
+          {data.tags ? (
+            <Grid
+              sx={{
+                position: "relative",
+                width: "100%",
+                display: "flex",
+                paddingLeft: "1%",
+                alignItems: "flex-start",
+                flexDirection: "row",
+              }}
+            >
+              <Stack
+                direction={"row"}
+                sx={{ flexWrap: "wrap", columnGap: "5px", rowGap: "5px" }}
+              >
+                {data.tags.map((tag, i) => (
+                  <Chip
+                    key={i}
+                    label={tag}
+                    color="primary"
+                    variant="outlined"
+                    onDelete={() => {
+                      setData((prev) => ({
+                        ...prev,
+                        tags: prev.tags.filter((t) => t !== tag),
+                      }));
+                    }}
+                  />
+                ))}
+              </Stack>
+            </Grid>
+          ) : (
+            <></>
+          )}
+          <Grid
+            sx={{
+              position: "relative",
+              paddingLeft: "1%",
             }}
-            src={video.src}
-          ></video>
+          >
+            🎵 {data.song.name}
+          </Grid>
+          <Grid
+            sx={{
+              position: "relative",
+              width: "100%",
+              height: "80%",
+              display: "flex",
+              justifyContent: "center",
+              flex: "1 1 auto",
+            }}
+          >
+            <video
+              muted
+              controls
+              style={{
+                maxWidth: "100%",
+                width: "auto",
+                height: "auto",
+                objectFit: "contain",
+              }}
+              src={data.src}
+            ></video>
+          </Grid>
         </Grid>
-      </Grid>
+      </ClickAwayListener>
     </ThemeProvider>
   );
 }
